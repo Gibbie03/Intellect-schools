52e8c6ba58826c121e12a92d007742f2431c466b
Gibbie03 <130116179+Gibbie03@users.noreply.github.com>
2026-08-07 18:56:49 +0100

    Refactor color calculation logic in colors.ts

--- lib/colors.ts
+++ lib/colors.ts
@@ -42,40 +42,6 @@ function rgbToHsl(r: number, g: number, b: number) {
   if (max !== min) {
     const d = max - min;
 
-    s =
-      l > 0.5
-        ? d / (2 - max - min)
-        : d / (max + min);
-
-    switch (max) {
-      case r:
-        h = (g - b) / d + (g < b ? 6 : 0);
-        break;
-
-      case g:
-        h = (b - r) / d + 2;
-        break;
-
-      case b:
-        h = (r - g) / d + 4;
-        break;
-    }
-
-    h /= 6;
-  }
-
-  return {
-    h: Math.round(h * 360),
-    s: Math.round(s * 100),
-    l: Math.round(l * 100),
-  };
-}  let h = 0;
-  let s = 0;
-  const l = (max + min) / 2;
-
-  if (max !== min) {
-    const d = max - min;
-
     s = l > 0.5
       ? d / (2 - max - min)
       : d / (max + min);
@@ -84,9 +50,11 @@ function rgbToHsl(r: number, g: number, b: number) {
       case r:
         h = (g - b) / d + (g < b ? 6 : 0);
         break;
+
       case g:
         h = (b - r) / d + 2;
         break;
+
       case b:
         h = (r - g) / d + 4;
         break;
@@ -100,35 +68,4 @@ function rgbToHsl(r: number, g: number, b: number) {
     s: Math.round(s * 100),
     l: Math.round(l * 100),
   };
-}  let h = 0;
-  let s = 0;
-  const l = (max + min) / 2;
-
-  if (max !== min) {
-    const d = max - min;
-
-    s = l > 0.5
-      ? d / (2 - max - min)
-      : d / (max + min);
-
-    switch (max) {
-      case r:
-        h = (g - b) / d + (g < b ? 6 : 0);
-        break;
-      case g:
-        h = (b - r) / d + 2;
-        break;
-      case b:
-        h = (r - g) / d + 4;
-        break;
     }
-
-    h /= 6;
-  }
-
-  return {
-    h: Math.round(h * 360),
-    s: Math.round(s * 100),
-    l: Math.round(l * 100),
-  };
-}
